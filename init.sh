4d3be0ca421bc7b25ab8b9aa4c86b9128f7ff1ab
Guillaume Pellerin <yomguy@parisson.com>
2015-03-20 18:41:37 +0100

    Add Telemeta submodules

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 git clone -b dev https://github.com/Parisson/TimeSide.git
-git clone -b dev https://github.com/Parisson/Telemeta.git
+git clone -b dev --recursive https://github.com/Parisson/Telemeta.git
 git clone -b dev https://github.com/ANR-DIADEMS/timeside-diadems.git
