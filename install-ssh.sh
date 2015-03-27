31deab50ef06b3df46413cad746a752ab6e8756f
Guillaume Pellerin <yomguy@parisson.com>
2015-03-27 21:58:42 +0100

    update names and doc, go to RST, add windows batch

--- install-ssh.sh
+++ install-ssh.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 git clone -b dev git@github.com:Parisson/TimeSide.git
 git clone -b dev git@github.com:Parisson/Telemeta.git
 git clone -b dev git@github.com:ANR-DIADEMS/timeside-diadems.git
---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+git clone -b dev https://github.com/Parisson/TimeSide.git
+git clone -b dev --recursive https://github.com/Parisson/Telemeta.git
+git clone -b dev https://github.com/ANR-DIADEMS/timeside-diadems.git
