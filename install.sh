47b4199db9e810e56cbcff35b0e7b87d4a62f1fa
Thomas Fillon <thomas@parisson.com>
2016-05-23 13:52:12 +0200

    Update install.sh to match TM and TS Diadems branches

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-git clone -b dev https://github.com/Parisson/TimeSide.git
-git clone -b dev --recursive https://github.com/Parisson/Telemeta.git
+git clone -b diadems https://github.com/Parisson/TimeSide.git
+git clone -b diadems --recursive https://github.com/Parisson/Telemeta.git
 git clone -b dev https://github.com/ANR-DIADEMS/timeside-diadems.git
